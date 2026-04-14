93bc65f3c86ee290304c15a5c05aa8ae16ad69e9
Harish Gangurde <harishgangurde24@gmail.com>
2026-04-14 18:13:22 +0530

    DataTypes

---
+++ dataTypes/Code10.py
@@ -0,0 +1,9 @@
+
+subList = ["Fundamentals in java","Microcontroller","Digital Communication","Electromagnetic Field Theory","Database Management"]
+
+print(subList)
+print(subList[0])
+print(subList[1])
+print(subList[2])
+print(subList[3])
+print(subList[4])
