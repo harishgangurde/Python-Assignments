1a17c7c2228a97958b6c3596d7e84d0750191880
Harish Gangurde <harishgangurde24@gmail.com>
2026-04-22 18:28:57 +0530

    DataTypes

---
+++ dataTypes/Code20.py
@@ -0,0 +1,22 @@
+
+intData = 33
+floatData = 44.5
+compData = 10+23j
+stringData = "Nico Hukenberg"
+listData = [10,20,30]
+tupleData = (10,20,30)
+dicData = {33:'Max',44:'Lewis'}
+setData = {10,30,60}
+flag = True
+val = None
+
+print(type(intData))
+print(type(floatData))
+print(type(compData))
+print(type(stringData))
+print(type(listData))
+print(type(tupleData))
+print(type(dicData))
+print(type(setData))
+print(type(flag))
+print(type(val))
