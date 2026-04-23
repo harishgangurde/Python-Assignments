199c48ce2a5ebdde8d35b84b22a07eb28557ce3f
Harish Gangurde <harishgangurde24@gmail.com>
2026-04-23 20:20:05 +0530

    If else

---
+++ if-else/code1.py
@@ -0,0 +1,8 @@
+
+x = int(input("Enter num1: "))
+y = int(input("Enter num2: "))
+
+if x<y:
+    print(y,"is the maximum number between",x,"&",y)
+else:
+    print(x,"is the maximum number between",x,"&",y)
