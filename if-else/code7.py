19f2192526bc36ed7d94e8f5c4053fa9dfcbb582
Harish Gangurde <harishgangurde24@gmail.com>
2026-04-25 16:35:07 +0530

    If else

---
+++ if-else/code7.py
@@ -0,0 +1,27 @@
+
+x = int(input("Enter number of month: "))
+
+if x == 1:
+    print("January is a 31-day month")
+elif x == 2:
+    print("February is a 28-day month")
+elif x == 3:
+    print("March is a 31-day month")
+elif x == 4:
+    print("April is a 30-day month")
+elif x == 5:
+    print("May is a 31-day month")
+elif x == 6:
+    print("June is a 30-day month")
+elif x == 7:
+    print("July is a 31-day month")
+elif x == 8:
+    print("August is a 31-day month")
+elif x == 9:
+    print("September is a 30-day month")
+elif x == 10:
+    print("October is a 31-day month")
+elif x == 11:
+    print("November is a 30-day month")
+elif x == 12:
+    print("December is a 31-day month")
