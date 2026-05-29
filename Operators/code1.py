a114ea17bbaac022d538608bfbf3e516726b9ce4
Harish Gangurde <harishgangurde24@gmail.com>
2026-05-29 21:42:20 +0530

    Operators

---
+++ Operators/code1.py
@@ -0,0 +1,11 @@
+
+x = 15
+y = 4
+
+print("Addition :",x+y)
+print("Subtraction :",x-y)
+print("Multiplication :",x*y)
+print("Division :",x/y)
+print("Modulus :",x%y)
+print("Floor Division :",x//y)
+print("Exponentiation :",x**y)
