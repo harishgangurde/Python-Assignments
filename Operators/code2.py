b7106cd0545dfe4b2965d8a8178a196a946ab143
Harish Gangurde <harishgangurde24@gmail.com>
2026-05-30 15:53:29 +0530

    Operators

---
+++ Operators/code2.py
@@ -0,0 +1,22 @@
+
+
+num = 50
+print("Initial Value :",num)
+
+num += 10 
+print("After += 10 :",num)
+
+num -= 5
+print("After -= 5 :",num)
+
+num *= 2
+print("After *= 2 :",num)
+
+num /= 4
+print("After /= 4 :",num)
+
+num %= 5 
+print("After %= 5 :",num)
+
+num //= 3
+print("After //= 3 :",num)
