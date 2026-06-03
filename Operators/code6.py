9bc6ece3e753911d4c2a69477870fb44ea924431
Harish Gangurde <harishgangurde24@gmail.com>
2026-06-03 21:59:56 +0530

    Operators

---
+++ Operators/code6.py
@@ -0,0 +1,7 @@
+
+color = ['red','blue','green','yellow']
+
+print("blue in color:",'blue' in color)
+print("purple in color:",'purple' in color)
+print("pink not in color:",'pink' not in color)
+print("red not in color:",'red' not in color)
