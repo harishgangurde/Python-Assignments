a384acdbd72e8b5f44a29bd124133568a91e23fb
Harish Gangurde <harishgangurde24@gmail.com>
2026-06-04 21:15:00 +0530

    Operators

---
+++ Operators/code7.py
@@ -0,0 +1,10 @@
+
+x = 12
+y = 10
+
+print("x&y(Bitwise AND):",x&y)
+print("x|y(Bitwise OR):",x|y)
+print("x^y(Bitwise XOR):",x^y)
+print("~x(Bitwise NOT):",~x)
+print("x<<2(Left shift by 2):",x<<2)
+print("x>>2(Right shift by two):",x>>2)
