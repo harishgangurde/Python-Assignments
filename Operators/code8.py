645fe1d2b63099a0551e6f7ddfffbc334436b9ff
Harish Gangurde <harishgangurde24@gmail.com>
2026-06-05 21:57:52 +0530

    Operators

---
+++ Operators/code8.py
@@ -0,0 +1,14 @@
+
+intVal = 10
+floatVal = 5.5
+stringVal = 'Python'
+
+print("Expected Ouput:")
+print("intVal = 10 Type:",type(intVal))
+print("floatVal = 5.5 Type:",type(floatVal))
+print("stringVal = 'Python' Type:",type(stringVal))
+
+print("intVal+floatVal = 15.5 Type:",type(intVal+floatVal))
+print("intVal*floatVal = 55.0 Type:",type(intVal*floatVal))
+print("floatVal/intVal = 0.55 Type:",type(floatVal/intVal))
+print("stringVal*2 = PythonPython Type:",type(stringVal*2))
