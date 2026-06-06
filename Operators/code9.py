78ffc78bb5ad3108cb9105389f55ae0c6d311776
Harish Gangurde <harishgangurde24@gmail.com>
2026-06-06 21:35:14 +0530

    Operators

---
+++ Operators/code9.py
@@ -0,0 +1,11 @@
+
+first = "Hello"
+second = "World"
+
+print("first =",first)
+print("second =",second)
+
+print("Concatenation (first+''+second):",first+' '+second)
+print("Repition (first*3):",first*3)
+print("Repition (second*2):",second*2)
+print("combined (first+'')*2+second:",(first+' ')*2+second)
