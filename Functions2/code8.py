ea137cfc160c76b618f7f98826bb4cc44185956f
Harish Gangurde <harishgangurde24@gmail.com>
2026-07-09 23:06:20 +0530

    Functions2

---
+++ Functions2/code8.py
@@ -0,0 +1,11 @@
+
+def Student(x,y,z):
+    print("Name:",x)
+    print("Age:",y)
+    print("Course:",z)
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+course = input("Enter course: ")
+
+Student(name,age,course)
